2ef41fb6227c2e40cf9c93067decc9378428acd2
Higor Matheus <hmoliveira83@gmail.com>
2025-05-23 18:33:13 -0300

    ajustes

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,26 +1,26 @@
 // 'use client';
 
-import { api, apiNext } from '@/config/api';
+import { apiNext } from '@/config/api';
 import { use } from 'react';
 
 export default async function UserPage() {
 	// use(async () => {
-	api.get('/users').then((res) => {
-		console.log('res axios', res.data);
-	});
+	// api.get('/users').then((res) => {
+	// 	console.log('res axios', res.data);
+	// });
 	// const data = await res.data;
 	// console.log(data);
 	// });
 
-	// apiNext('/users', {
-	// 	method: 'GET',
-	// })
-	// 	.then((res) => {
-	// 		console.log('res', res);
-	// 	})
-	// 	.catch((err) => {
-	// 		console.log('err', err);
-	// 	});
+	apiNext('/users', {
+		method: 'GET',
+	})
+		.then((res) => {
+			console.log('res', res);
+		})
+		.catch((err) => {
+			console.log('err', err);
+		});
 	return (
 		<div>
 			<h1>Users</h1>
